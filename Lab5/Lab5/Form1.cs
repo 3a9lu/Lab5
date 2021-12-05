85bec7f18d2cb63510760d9b93c86d2aa1247aea
3a9lu <eduard.grinvald@yandex.ru>
2021-12-05 16:07:28 +0700

    Всё готово.

--- Lab5/Lab5/Form1.cs
+++ Lab5/Lab5/Form1.cs
@@ -1,107 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using org.mariuszgromada.math.mxparser;
+using Wolfram.Alpha;
+using Wolfram.Alpha.Models;
 
 namespace Lab5
 {
     public partial class Form1 : Form
     {
+        public string function;
+        public double a;
+        public double b;
+        public double eps;
         public Form1()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;  // Вывод формы по центру экрана
         }
 
-        // Блокировка лишнних символов в TextBox
-        #region
-        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            char ch = e.KeyChar;
-            int length = textBox1.Text.Length;
-            if (length == 0 && ch == ',' && ch == '-')
-            {
-                e.Handled = true;
-            }
-            if (!Char.IsDigit(ch) && ch != 8 && (ch != ',' || textBox1.Text.Contains(",")) && ((ch != '-' || textBox1.Text.Contains("-")))) // Если число, BACKSPACE запятая или минус, то вводим
-            {
-                e.Handled = true;
-            }
-        }
-        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            char ch = e.KeyChar;
-            int length = textBox1.Text.Length;
-            if (length == 0 && ch == ',' && ch == '-')
-            {
-                e.Handled = true;
-            }
-            if (!Char.IsDigit(ch) && ch != 8 && (ch != ',' || textBox1.Text.Contains(",")) && ((ch != '-' || textBox1.Text.Contains("-")))) // Если число, BACKSPACE запятая или минус, то вводим
-            {
-                e.Handled = true;
-            }
-        }
-        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            char ch = e.KeyChar;
-            int length = textBox1.Text.Length;
-            if (length == 0 && ch == ',' && ch == '-')
-            {
-                e.Handled = true;
-            }
-            if (!Char.IsDigit(ch) && ch != 8 && (ch != ',' || textBox1.Text.Contains(",")) && ((ch != '-' || textBox1.Text.Contains("-")))) // Если число, BACKSPACE запятая или минус, то вводим
-            {
-                e.Handled = true;
-            }
-        }
-        #endregion
         private void выйтиToolStripMenuItem_Click(object sender, EventArgs e) // Кнопка выхода
         {
             Close();
         }
-        void Clear()
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e) // Кнопка очистить
         {
-            textBox1.Text = "";
-            
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart3.Series[0].Points.Clear();
+
+            chart1.Series[1].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            chart3.Series[1].Points.Clear();
+            richTextBox1.Clear();
+        }
+
+        public async Task Wolf(string function)
+        {
+            WolframAlphaService service = new WolframAlphaService("E6JG63-6UYTPQPJGG");
+            WolframAlphaRequest request = new WolframAlphaRequest($"Integral {function}");
+            WolframAlphaResult result = await service.Compute(request);
+            string integral = result.QueryResult.Pods[0].SubPods[0].Plaintext;
+            richTextBox1.AppendText($"{integral} \n");
+        }
+        public void ChartDob()
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart3.Series[0].Points.Clear();
+            Function Function = new Function(function);
+            for (double i = a; i <= b; i += 0.1)
+            {
+                for (int j = 0; j < chart1.Series.Count; j += 2)
+                {
+                    chart1.Series[0].Points.AddXY(i, Function.calculate(i));
+                    chart2.Series[0].Points.AddXY(i, Function.calculate(i));
+                    chart3.Series[0].Points.AddXY(i, Function.calculate(i));
+                }
+            }
+        }
+        public void ChartCl()
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
             chart1.Series[0].Points.Clear();
             chart2.Series[0].Points.Clear();
             chart3.Series[0].Points.Clear();
         }
-        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e) // Кнопка очистить
+
+        private void выбратьМетодыToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Clear();
+            Form2 Select = new Form2(this, chart1, chart2, chart3, richTextBox1);
+            Select.Show();
         }
-        private void расчитатьToolStripMenuItem_Click(object sender, EventArgs e)
+        private void функцияToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Clear();
-            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
-            {
-                MessageBox.Show("Заполните все поля.", "Ошибка!");
-                return;
-            }
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart3.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            chart3.Series[1].Points.Clear();
+            richTextBox1.Clear();
 
-            if (Convert.ToDouble(textBox2.Text) >= Convert.ToDouble(textBox3.Text))
-            {
-                MessageBox.Show("Граница A не может быть равна границе B или быть больше.", "Ошибка!");
-                return;
-            }
-
-            if (Convert.ToDouble(textBox4.Text) <= 0)
-            {
-                MessageBox.Show("Точность не может быть равна нулю или быть меньше.", "Ошибка!");
-                return;
-            }
-
-            else
-            {
-
-            }
+            Form3 Function = new Form3(this);
+            Enabled = false;
+            Function.Show();
+        }
+    }
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
         }
     }
 }
